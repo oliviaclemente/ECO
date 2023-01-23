4e0f6dd181a71796fd7c9e9ccd173cf91d60b01a
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-01-23 09:28:57 +0000

    cc

--- ej6.py
+++ ej6.py
@@ -2,4 +2,4 @@ def valor(palabra):
   palabra= palabra.upper()
   palabra_lista= list(permutaciones(palabra))
   palabra_lista sorted(palabra_lista)
-  return palabra_lista.index(tuple(palabra))
+  return palabra_lista.index(tuple(palabra)) +1
---
+++ ej8.py
@@ -0,0 +1,58 @@
+def comparar(a, b)
+  if a == b:
+    return 0
+  elif a == "12 bateristas tocando el tambor"
+    return -1
+  elif b == "12 bateristas tocando el tambor"
+    return 1
+  elif a == "tubería de 11 gaiteros"
+    return  -1
+   elif b == "tubería de 11 gaiteros"
+    return  1
+  elif a == "10 señores un salt,":
+        return -1
+    elif b == "10 señores un salt,":
+        return 1
+    elif a == "9 damas bailando,,":
+        return -1
+    elif b == "9 damas bailando,,":
+        return 1
+    elif a == "8 criadas un ordeñ,":
+        return -1
+    elif b == "8 criadas un ordeñ,":
+        return 1
+    elif a == "7 cisnes nadando,":
+        return -1
+    elif b == "7 cisnes nadando,":
+        return 1
+    elif a == "6 gansos una puesta,":
+        return -1
+    elif b == "6 gansos una puesta":
+        return 1
+    elif a == "5 anillos de oro,,":
+        return -1
+    elif b == "5 anillos de oro,":
+        return 1
+    elif a == "4 pájaros cantando,":
+        return -1
+    elif b == "4 pájaros cantando,":
+        return 1
+    elif a == "3 gallinas francesas,,":
+        return -1
+    elif b == "3 gallinas francesas,":
+        return 1
+    elif a == "2 tórtolas":
+        return -1
+    elif b == "2 tórtolas":
+        return 1
+    elif a == "1 perdiz en un peral.":
+        return -1
+    elif b == "1 perdiz en un peral.":
+        return 1
+    else: 
+        return 0
+def ordenar(versos)
+  return ordenar(versos, cmp=comparar)
+
+
+
