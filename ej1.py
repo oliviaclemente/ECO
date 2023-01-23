a97a29d5c1f6a3eb7e2e80d13294d3f732c75ac9
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-01-23 09:04:12 +0000

    c

--- ej1.py
+++ ej1.py
@@ -6,3 +6,14 @@ return lista
 def_ordenarlista_menor_mayor(lista):
 lista.sort()
 return lista
+
+def_ordenar_lista_pares_primero(lista):
+listasort(reverse=True)
+lista_pares=[]
+lista_impares=[]
+for num in lista:
+  if num % 2==0
+  lista_pares.append(num)
+else:
+lista_impreas.append(num)
+  
