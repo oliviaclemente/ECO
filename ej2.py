a9754a7a8c07b21ef51a2c96d7184279e17396ce
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-01-23 09:11:52 +0000

    cc

--- ej2.py
+++ ej2.py
@@ -3,4 +3,11 @@ def db(links):
   record= [0, 0]
   for l in links:
 if l[0] ==  if l[0] == l[1] - 1:
-  
+  if red[l[0] // 3][l[0] % 3] == 0 and red[l[1] // 3][l[1] % 3] == 0:
+    record[0] += 1
+    record[1] += 1
+  elif red[l[0] // 3][l[0] % 3] == 0:
+    record[0] += 1
+   elif red[l[1] // 3][l[1] % 3] == 0:
+     record[1] += 1
+    
