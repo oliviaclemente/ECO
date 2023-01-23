881bf733079c292b903eb25f62f55f52b53348c6
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-01-23 09:08:37 +0000

    ccc

--- ej1.py
+++ ej1.py
@@ -1,21 +1,29 @@
 def ordenar_lista(lista):
   lista.sort(reverse=True)
-return lista
+  return lista
 
 
 def_ordenarlista_menor_mayor(lista):
-lista.sort()
-return lista
+  lista.sort()
+  return lista
 
 def_ordenar_lista_pares_primero(lista):
-listasort(reverse=True)
-lista_pares=[]
-lista_impares=[]
-for num in lista:
-  if num % 2==0
-  lista_pares.append(num)
-else:
-lista_impreas.append(num)
- return lista_pares + lista_impares
+  listasort(reverse=True)
+  lista_pares=[]
+  lista_impares=[]
+  for num in lista:
+    if num % 2==0
+    lista_pares.append(num)
+  else:
+   lista_impreas.append(num)
+   return lista_pares + lista_impares
 
-def oredenar_lista
+def oredenar_lista_paresu(lista):
+  lista.sort(reverse=True)
+  lista_pares=[]
+  lista_impares=[]
+  for num in lista:
+   if num % 2==0
+    lista_pares.append(num)
+  else:
+    lista_impreas.append(num)
---
+++ ej2.py
@@ -0,0 +1,3 @@
+def db(links):
+  red= [[0 for _ in range(3)] for _ in range(3)]
+  record= 
